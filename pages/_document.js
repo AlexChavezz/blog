4ee9340c2f80239a8f83a9d0483f1218c08cbde3
AlexChavezz <alexischavez1234.ac@gmail.com>
2022-10-24 11:25:31 -0500

    fonts-load-issue fixed

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,6 @@ export default function Document() {
         <Html>
             <Head>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional" />
-                {/* GOOGLE FONTS */}
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                <link href="https://fonts.googleapis.com/css2?family=Inter+Tight&display=optional" rel="stylesheet" />
             </Head>
             <body>
                 <Main />
