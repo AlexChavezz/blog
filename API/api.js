31bb89ef6ab2be574e6dbdcb862d21ec99766e80
AlexChavezz <76461142+AlexChavezz@users.noreply.github.com>
2023-07-19 00:18:04 -0600

    Update api.js
    new api endpoint

--- API/api.js
+++ API/api.js
@@ -1,2 +1,2 @@
-export const URL_API = 'https://blog-rest-server.azurewebsites.net/api';
+export const URL_API = 'https://alexisblogapp.azurewebsites.net/api';
 // export const URL_API = 'http://localhost:8080/api';
